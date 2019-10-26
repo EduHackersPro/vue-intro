c6d8ccdae14b0bc84d715d3d0ed75cd0b2085c2a
Roman Spiridonov <rom@nspiridonov.ru>
2019-10-26 17:13:14 +0300

    Intro to props, events and data rendering

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Добро пожаловать на курс по Vue.js"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <hello-world :msg="modifiedMessage" @update="update" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
+
   components: {
     HelloWorld
+  },
+
+  data: () => ({
+    message: "Добро пожаловать на курс по Vue.js!!!",
+    yourName: ""
+  }),
+
+  computed: {
+    modifiedMessage() {
+      if (this.yourName?.length > 0) {
+        return `${this.yourName}, добро пожаловать на курс по Vue.js!!!`;
+      }
+      return "Добро пожаловать на курс по Vue.js!!!";
+    }
+  },
+
+  methods: {
+    update(_yourName) {
+      this.yourName = _yourName;
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
