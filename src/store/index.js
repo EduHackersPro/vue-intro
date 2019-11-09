1ea3d5a109a4d1d5dcfd74b10908a65b06205f61
Roman Spiridonov <rom@nspiridonov.ru>
2019-11-09 22:52:46 +0300

    NPM INSTALL needed Добавлен vuex-persistedstate Исправлено хранение модулей внутри store

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import auth from './modules/auth'
 Vue.use(Vuex)
+
+import auth from './modules/auth'
+import todo from './modules/todo'
+
+import createPersisteState from 'vuex-persistedstate'
 
 const debug = process.env.NODE_ENV !== 'production'
 
 export default new Vuex.Store({
-  modules: [auth],
-  plugins: [],
+  modules: { auth, todo },
+  plugins: [createPersisteState()],
   strict: debug,
 })
