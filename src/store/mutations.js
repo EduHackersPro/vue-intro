cec2023185effa2d4b46fecb639bd4fe8e303cd5
MaxAizy <57674948+MaxAizy@users.noreply.github.com>
2019-11-20 21:02:04 +0300

    Update mutations.js

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -3,4 +3,5 @@ export const SAVE_API_TOKEN = 'SAVE_API_TOKEN'
 export const DEL_API_TOKEN = 'DEL_API_TOKEN'
 
 // todo
-export const TODO_ADD_ITEM = 'TODO_ADD_ITEM'
+export const TODO_ADD_TASK = "TODO_ADD_TASK";
+export const TODO_CHANGESTATE_TASK = "TODO_CHANGESTATE_TASK";
